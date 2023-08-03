82fc1dbc99fa248240ce0391ccc037bd9a8f9512
raunakpurohit <raunak.wscubetech@gmail.com>
2023-08-03 21:39:09 +0530

    Home UI Module completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green.shade100,
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
 
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
